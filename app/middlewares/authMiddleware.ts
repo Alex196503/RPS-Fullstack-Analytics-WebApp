55390ac2249b6b3091d2ffccfa33ee6f2f56b0c2
Alex196503 <alexmoldovan2009@gmail.com>
2026-07-17 13:13:58 +0300

    fixed some errors

--- app/middlewares/authMiddleware.ts
+++ app/middlewares/authMiddleware.ts
@@ -12,13 +12,7 @@ export const authentificationMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  let token = req.cookies.token as string | undefined
-  if (!token && req.headers.authorization) {
-    const parts = req.headers.authorization.split(" ")
-    if (parts.length === 2 && parts[0] === "Bearer") {
-      token = parts[1]
-    }
-  }
+  const token = req.cookies.token as string | undefined
   if (!token) {
     return res
       .status(401)
